c59841835695b992c58f5f5ca5f127d1b6df4c72
mohammad.estaytieh <mohammad.estaytieh@cardinalhealth.com>
2022-06-29 16:24:23 +0300

    [0.0.0.1] data setup : fixing schema and data creation for syntax errors

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,11 +2,10 @@ CREATE SCHEMA bank;
 
 CREATE TABLE bank.usertype (
     id bigint NOT NULL PRIMARY KEY,
-    usertype CHAR(100) NOT NULL,
-    UNIQUE (id, usertype)
+    usertype varchar(100) NOT NULL
 );
 
-CREATE TABLE bank.Address (
+CREATE TABLE bank.address (
     id bigint NOT NULL PRIMARY KEY,
     country varchar(100) not null,
     province varchar(100) not null,
@@ -16,27 +15,28 @@ CREATE TABLE bank.Address (
     address varchar (400) not null
 );
 
-CREATE TABLE bank.user (
+CREATE TABLE bank.userinfo (
     id bigint NOT NULL PRIMARY KEY,
-    username CHAR(100) NOT NULL,
-    fullname CHAR(200) NOT NULL,
-    usertype CHAR(100) NOT NULL REFERENCES bank.usertype(usertype)
-    passwordHash CHAR(200) NOT NULL,
-    pepper CHAR(50) NOT NULL,
-    salt CHAR (100) NOT NULL,
+    username varchar(100) NOT NULL,
+    firstname varchar(200) NOT NULL,
+    surname varchar(200) NOT NULL,
+    usertype varchar(100) NOT NULL,
+    passwordHash varchar(200) NOT NULL,
+    pepper varchar(50) NOT NULL,
+    salt varchar (100) NOT NULL,
     bankname VARCHAR(50) NOT NULL,
     email VARCHAR (200),
     phone VARCHAR (30),
-    address bigint REFERENCES bank.address(id)
-    UNIQUE (username)
+    address bigint REFERENCES bank.address(id),
+    UNIQUE (username,bankname)
 );
 
 CREATE TABLE bank.account (
     id bigint NOT NULL PRIMARY KEY,
-    accountnumber CHAR(10) NOT NULL,
-    currentbalance NUMERIC(10,3) NOT NULL,
+    accountnumber varchar(10) NOT NULL,
+    currentbalance NUMERIC(100,3) NOT NULL,
     bankname VARCHAR(50) NOT NULL,
-    userid bigint not null REFERENCES bank.user(id),
+    userid bigint not null REFERENCES bank.userinfo(id),
     UNIQUE (accountnumber)
 );
 
@@ -45,8 +45,8 @@ CREATE TABLE bank.transaction (
     id bigint NOT NULL PRIMARY KEY,
     sourceaccountid bigint REFERENCES bank.account(id),
     targetaccountid bigint REFERENCES bank.account(id),
-    amount NUMERIC(10,3) NOT NULL,
+    amount NUMERIC(100,3) NOT NULL,
     initiationdate timestamp NOT NULL,
     completiondate TIMESTAMP,
-    transactionreason VARCHAR(255)
+    transactionreason VARCHAR(500)
 );
